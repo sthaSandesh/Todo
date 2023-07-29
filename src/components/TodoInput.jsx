e482caae53c9ec1be8420f40a816dc6dcf13db22
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-29 18:24:23 +0545

    input componnet add

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TodoInput() {
+  return (
+    <div>
+        <input 
+        type="text" 
+        placeholder='Enter Your Task Here'
+        className='rounded-lg border-2 border-purple-600 p-2 m-2 w-80 text-purple-600 font-bold'
+        />
+        <button
+        className='bg-purple-600 text-white p-2 rounded-lg font-bold'
+        >Add</button>
+    </div>
+  )
+}
+
+export default TodoInput
