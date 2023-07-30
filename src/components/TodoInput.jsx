1ed0f5817ef9737f58cd8e68fd9b8222c28bbc0f
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-30 22:15:35 +0545

    little change

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
 
 function TodoInput(pros) {
-  const [inputText, setInputText] = useState('') 
+  const [inputText, setInputText] = useState('')
 
 
   return (
     <div>
-        <input 
-        type="text" 
+      <input
+        type="text"
         placeholder='Enter Your Task Here'
         className='rounded-lg border-2 border-purple-600 p-2 m-2 w-80 text-purple-600 font-bold'
         onChange={(e) => setInputText(e.target.value)}
-        />
-        <button
+      />
+      <button
         onClick={() => {
           pros.addTask(inputText)
           setInputText('')
         }}
         className='bg-purple-600 text-white p-2 rounded-lg font-bold'
-        >Add</button>
+      >Add</button>
     </div>
   )
 }
