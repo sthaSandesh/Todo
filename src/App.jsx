26f14fc24d36c66147e15b2471c1341f9b7d986e
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-08-01 22:09:19 +0545

    style change

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ function App() {
 
   return (
     <div >
+      <div className='bg-purple-700 text-white font-bold text-4xl'>
+        <h1 className='ml-8 py-2'>TODO</h1>
+      </div>
       <TodoInput addTask={addTask} />
 
-      <h1 className='font-bold text-5xl text-purple-700 py-4'>
-        Todo
+      <h1 className='font-bold text-2xl text-white py-4 mx-8'>
+        Your Tasks
         <hr />
         {List.map((task, index) => {
           return (
