5f43d7036d58e79853b276c1254f10862b7afb10
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-08-01 22:16:05 +0545

    little change

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -6,23 +6,23 @@ function TodoInput(props) {
 
   return (
     <div className='flex items-center justify-center py-2'>
-  <div>
-    <input
-      type="text"
-      placeholder='Enter Your Task Here'
-      value={inputText}
-      className='rounded-lg border-2 border-purple-600 p-2 m-2 w-80 text-purple-600 font-bold'
-      onChange={(e) => {setInputText(e.target.value)}}
-    />
-    <button
-      onClick={() => {
-        props.addTask(inputText)
-        setInputText("")
-      }}
-      className='bg-purple-600 text-white p-2 rounded-lg font-bold'
-    >Add</button>
-  </div>
-</div>
+      <div>
+        <input
+          type="text"
+          placeholder='Enter Your Task Here'
+          value={inputText}
+          className='rounded-lg border-2 border-purple-600 p-2 m-2 w-80 text-purple-600 font-bold'
+          onChange={(e) => { setInputText(e.target.value) }}
+        />
+        <button
+          onClick={() => {
+            props.addTask(inputText)
+            setInputText("")
+          }}
+          className='bg-purple-600 text-white p-2 rounded-lg font-bold'
+        >Add</button>
+      </div>
+    </div>
 
   )
 }
